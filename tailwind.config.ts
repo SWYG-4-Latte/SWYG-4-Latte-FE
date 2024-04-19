da1fb9776a62b77191486dafc2a4a751db8fb324
jakesungjaekim@gmail.com <jakesungjaekim@gmail.com>
2024-04-19 21:34:27 +0900

    chore: BoilerPlate create

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,62 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/**/*.{js,ts,jsx,tsx}',
   ],
+  daisyui: {
+    themes: ['light', 'dark', 'cupcake'],
+  }, 
   theme: {
+    screens: {
+      xs: '460px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
+      colors: {
+        gray00: '#fff',
+        gray01: '#FCFAF8',
+        gray02: '#F7F5F3',
+        gray03: '#F1EFED',
+        gray04: '#E3E1DF',
+        gray05: '#C0BFBD',
+        gray06: '#A2A09E',
+        gray07: '#787775',
+        gray08: '#646361',
+        gray09: '#454342',
+        gray10: '#242221', 
+        gray11: '#000000',
+
+        // gray10 , gray08, gray06 === text01, text02, text03
+        // gray01 , gray02, gray03 === bg01, bg02, bg03
+
+        orange01: '#FDF3E4',
+        orange02: '#FBDFBD',
+        orange03: '#F9CB94',
+        orange04: '#F5B76E',
+        orange05: '#F4A857',
+        orange06: '#F39945',
+        orange07: '#EE8E41',
+        orange08: '#E6803E',
+        orange09: '#DF7238',
+        orange10: '#D45c31',
+
+        primaryAmber: '#F6B66E',
+        primaryOrange: '#F39945',
+        primaryBeige: '#FCFAF8',
+        primaryIvory: '#FCFAF8',
+        primaryDark: '#242221',
+        primaryRed: '#EB5252',
+      },
+      fontFamily: {
+        pretendard: 'Pretendard',
+        pretendard500: 'Pretendard-medium',
+        pretendard600: 'Pretendard-semiBold',
+        pretendard700: 'Pretendard-bold',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +65,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
