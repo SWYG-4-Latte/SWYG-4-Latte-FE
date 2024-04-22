2ac45a9838b5020bc043c95d4afe89c9ccd80129
sohyun <kshyun0215@gmail.com>
2024-04-22 11:50:40 +0900

    feat: navigation 관련 type과 상수 분리

--- src/components/BottomNavigation/NavItem.tsx
+++ src/components/BottomNavigation/NavItem.tsx
@@ -3,33 +3,15 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import HomeIcon from '../icons/HomeIcon';
-import CategoryIcon from '../icons/CategoryIcon';
-import CalendarIcon from '../icons/CalendarIcon';
-import ArticleIcon from '../icons/ArticleIcon';
-
-interface Icon {
-  [key: string]: ({ className }: { className: string }) => JSX.Element;
-}
-
-interface NavItemProps {
-  path: string;
-  name: string;
-}
-
-const ICON: Icon = {
-  home: HomeIcon,
-  category: CategoryIcon,
-  calendar: CalendarIcon,
-  article: ArticleIcon,
-};
+import { NavItemProps } from '@/types/navigation';
+import { NAV_ICON } from '@/constants/navIcon';
 
 const NavItem = ({ path, name }: NavItemProps) => {
   const pathname = usePathname();
 
   const isActive = path === pathname;
   const color = isActive ? 'text-orange09' : 'text-gray06';
-  const NavIcon = ICON[path.slice(1)];
+  const NavIcon = NAV_ICON[path.slice(1)];
 
   return (
     <div className="flex w-[68px] justify-center">
---
+++ src/constants/navIcon.ts
@@ -0,0 +1,12 @@
+import HomeIcon from '@/components/icons/HomeIcon';
+import CategoryIcon from '@/components/icons/CategoryIcon';
+import CalendarIcon from '@/components/icons/CalendarIcon';
+import ArticleIcon from '@/components/icons/ArticleIcon';
+import { Icon } from '@/types/navigation';
+
+export const NAV_ICON: Icon = {
+  home: HomeIcon,
+  category: CategoryIcon,
+  calendar: CalendarIcon,
+  article: ArticleIcon,
+};
