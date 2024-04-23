5f64dd999fddc92f5a7d1bb2e1eb1a9d856c2bb1
sohyun <kshyun0215@gmail.com>
2024-04-23 23:36:46 +0900

    feat: 뒤로가기 버튼이 있는 NavigationHeader 구현

---
+++ src/components/common/header/NavigationHeader.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const NavigationHeader = ({ title }: { title?: string }) => {
+  const router = useRouter();
+
+  return (
+    <header className="fixed z-10 flex h-14 w-full max-w-[360px] items-center justify-center bg-primaryIvory">
+      <button className="absolute left-4">
+        <Image src="/svgs/arrow-left.svg" width={24} height={24} alt="뒤로 가기" onClick={() => router.back()} />
+      </button>
+      {title && <span className="font-medium text-gray10">{title}</span>}
+    </header>
+  );
+};
+
+export default NavigationHeader;
