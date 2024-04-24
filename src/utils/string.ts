c8c181d801107059d6684e9007f2939b9ece27a5
sohyun <kshyun0215@gmail.com>
2024-04-24 22:46:15 +0900

    feat: ellipsisText 유틸 함수 공백 포함 -> 미포함 글자수로 수정

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,17 @@
-export const ellipsisText = (str: string, length: number) => {
-  return str.length <= length ? str : str.substring(0, length) + '...';
+// 공백 미포함 글자 수 maxLength까지만 노출하고 뒤에 말줄임표 표시
+export const ellipsisText = (text: string, maxLength: number) => {
+  if (text.length <= maxLength) return text;
+
+  let slicedText = '';
+  let len = 0;
+
+  for (const ch of text) {
+    if (len >= maxLength) break;
+    if (ch != ' ') {
+      len += 1;
+    }
+    slicedText += ch;
+  }
+
+  return slicedText + '...';
 };
