77b848bff32ee5cc6c7019b2b590fbb225341932
sohyun <kshyun0215@gmail.com>
2024-04-26 14:03:07 +0900

    feat: 카페인 달력 경로 수정

---
+++ src/app/caffeine-calendar/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function CaffeineCalendarPage() {
+  return (
+    <main>
+      <header className="flex h-14 items-center justify-between bg-primaryIvory px-5">
+        <div className="text-lg font-semibold text-gray10">카페인 달력</div>
+        <Link href="today-caffeine">
+          <Image src="/svgs/today-caffeine.svg" width={24} height={24} alt="오늘 마신 카페인 보기" />
+        </Link>
+      </header>
+    </main>
+  );
+}
