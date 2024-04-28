75646f2c1f392a893fae4cf3fcc8549cfc12506a
sohyun <kshyun0215@gmail.com>
2024-04-28 20:01:11 +0900

    feat: NavigationHeader에서 children 받을 수 있게 수정

--- src/components/common/header/NavigationHeader.tsx
+++ src/components/common/header/NavigationHeader.tsx
@@ -2,16 +2,18 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { PropsWithChildren } from 'react';
 
-const NavigationHeader = ({ title }: { title?: string }) => {
+const NavigationHeader = ({ title, children }: PropsWithChildren<{ title?: string }>) => {
   const router = useRouter();
 
   return (
-    <header className="fixed z-10 flex h-14 w-full max-w-[500px] items-center justify-center bg-primaryIvory">
+    <header className="fixed z-10 flex h-14 w-full max-w-[500px] items-center justify-center bg-primaryIvory pr-5">
       <button className="absolute left-4">
         <Image src="/svgs/arrow-left.svg" width={24} height={24} alt="뒤로 가기" onClick={() => router.back()} />
       </button>
       {title && <span className="font-medium text-gray10">{title}</span>}
+      {children}
     </header>
   );
 };
