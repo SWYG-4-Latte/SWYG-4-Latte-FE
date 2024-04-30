3893a5dbc92382c0cb10f7eef547653f909f1d98
sohyun <kshyun0215@gmail.com>
2024-04-30 19:02:41 +0900

    fix: 최근 검색어에 있는 단어 검색 시 해당 단어 제일 앞에 오도록 수정

--- src/store/recentSearchStore.ts
+++ src/store/recentSearchStore.ts
@@ -16,9 +16,19 @@ export const useRecentSearchStore = create<RecentSearchState & RecentSearchActio
     (set) => ({
       searchList: [],
       addSearchWord: (word: string) =>
-        set((state) => ({
-          searchList: state.searchList.includes(word) ? state.searchList : [word, ...state.searchList.slice(0, 2)],
-        })),
+        set((state) => {
+          const existingWordIndex = state.searchList.indexOf(word);
+          // 이미 최근 검색어에 있는 경우 해당 단어 제거 후 맨 앞에 추가
+          if (existingWordIndex !== -1) {
+            const newList = state.searchList.filter((searchWord) => searchWord !== word);
+            newList.unshift(word);
+            return {
+              searchList: newList,
+            };
+          } else {
+            return { searchList: [word, ...state.searchList.slice(0, 2)] };
+          }
+        }),
       deleteSearchWord: (word: string) =>
         set((state) => ({
           searchList: state.searchList.filter((searchWord) => searchWord !== word),
