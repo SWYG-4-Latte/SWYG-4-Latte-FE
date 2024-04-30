07b191634e01063dfc17f1e8b912232360618f4d
sohyun <kshyun0215@gmail.com>
2024-04-30 23:16:31 +0900

    fix: 최근 검색어 삭제 버튼 핸들러에 이벤트 전파 막도록 stopPropagation 추가

--- src/components/search/RecentSearchItem.tsx
+++ src/components/search/RecentSearchItem.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { MouseEvent } from 'react';
 
 import { useRecentSearchStore } from '@/store/recentSearchStore';
 
@@ -14,13 +15,18 @@ const RecentSearchItem = ({ word }: { word: string }) => {
     router.replace(`/search?${params.toString()}`, { scroll: false });
   };
 
+  const handleDelete = (e: MouseEvent) => {
+    e.stopPropagation();
+    deleteRecentSearchWord(word);
+  };
+
   return (
     <li
       onClick={handleSearch}
       className="flex h-[50px] cursor-pointer justify-between border-b border-gray04 px-5 py-4 last:border-none"
     >
       <div className="text-sm text-gray08">{word}</div>
-      <button onClick={() => deleteRecentSearchWord(word)}>
+      <button onClick={handleDelete}>
         <Image src="/svgs/delete-recent-search.svg" width={16} height={16} alt="최근 검색어 삭제" />
       </button>
     </li>
