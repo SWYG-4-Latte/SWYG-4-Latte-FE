542d9e7af050b3902dda9a1dfa68f1c1506b3ec1
sohyun <kshyun0215@gmail.com>
2024-05-02 18:26:32 +0900

    feat: 로고 사이즈, 빈 음료 카드 스타일 수정

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -4,8 +4,7 @@ import Link from 'next/link';
 const HomeHeader = () => {
   return (
     <header className="fixed left-0 right-0 z-10 mx-auto flex h-14 max-w-[500px] justify-between space-x-44 bg-primaryIvory pl-5 pr-[22px]">
-      {/* 로고 이미지 변경 예정 */}
-      <Image src="/svgs/logo.svg" width={78} height={40} alt="로고" className="my-2" />
+      <Image src="/svgs/logo.svg" width={98.4} height={32} alt="로고" className="my-3" />
       <div className="my-4 flex gap-4">
         <Link href="/search" scroll={false}>
           <Image src="/svgs/search.svg" width={24} height={24} alt="검색 아이콘" />
--- src/components/home/drinkHistory/EmptyCard.tsx
+++ src/components/home/drinkHistory/EmptyCard.tsx
@@ -6,7 +6,7 @@ const EmptyCard = () => {
     <div className="w-full px-5 pb-8">
       <div className="h-[118px] overflow-hidden rounded-lg border border-gray04 bg-primaryIvory shadow-toast">
         <DrinkHistoryDetail heading="아직 마신 음료가 없어요" description="음료를 기록하고 카페인 정보를 받아보세요." />
-        <div className=" mx-auto h-px bg-gray04" />
+        <div className="mx-auto h-px w-[calc(100%-8px)] bg-gray04" />
         <DrinkHistoryCardFooter isEmpty />
       </div>
     </div>
