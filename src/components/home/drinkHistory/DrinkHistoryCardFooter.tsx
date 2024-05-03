8c086e1aac3ad6a5d3606ce06a79cfb519a2908e
sohyun <kshyun0215@gmail.com>
2024-05-03 20:44:29 +0900

    feat: 홈 - 최근 마신 음료 카드의 비교함 담기 기능 구현
    비교함에 담고 트스트 메시지 출력하거나 비교함으로 이동 또는 비교함 비우기

--- src/components/home/drinkHistory/DrinkHistoryCardFooter.tsx
+++ src/components/home/drinkHistory/DrinkHistoryCardFooter.tsx
@@ -1,13 +1,17 @@
-import DeleteComparisonBoxModal from '@/components/common/modal/DeleteComparisonBoxModal';
-import useModal from '@/hooks/useModal';
+interface DrinkHistoryCardFooterProps {
+  isEmpty: boolean;
+  onCompare: () => void;
+  onRecord: () => void;
+}
 
-const DrinkHistoryCardFooter = ({ isEmpty }: { isEmpty: boolean }) => {
-  const { isOpen, openModal, closeModal } = useModal();
-
+const DrinkHistoryCardFooter = ({ isEmpty, onCompare, onRecord }: DrinkHistoryCardFooterProps) => {
   if (isEmpty) {
     return (
       <div className=" flex h-[37px] w-full items-center justify-center">
-        <button className="h-full w-full text-xs text-gray10 hover:bg-orange01 hover:text-primaryOrange">
+        <button
+          onClick={onRecord}
+          className="h-full w-full text-xs text-gray10 hover:bg-orange01 hover:text-primaryOrange"
+        >
           카페인 기록하러 가기
         </button>
       </div>
@@ -16,12 +20,13 @@ const DrinkHistoryCardFooter = ({ isEmpty }: { isEmpty: boolean }) => {
 
   return (
     <div className="flex h-[37px] items-center text-xs">
-      <button className="h-[37px] w-[114px] text-gray10 hover:bg-orange01 hover:text-primaryOrange" onClick={openModal}>
+      <button onClick={onCompare} className="h-[37px] w-[114px] text-gray10 hover:bg-orange01 hover:text-primaryOrange">
         비교하기
       </button>
       <div className="h-[21px] w-px bg-gray04" />
-      <button className="h-[37px] w-[114px] text-orange09 hover:bg-orange01">기록하기</button>
-      <DeleteComparisonBoxModal isOpen={isOpen} onClose={closeModal} />
+      <button onClick={onRecord} className="h-[37px] w-[114px] text-orange09 hover:bg-orange01">
+        기록하기
+      </button>
     </div>
   );
 };
