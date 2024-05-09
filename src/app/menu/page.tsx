fa652dce76e0a20e0078848e4cac40728a7874ce
sohyun <kshyun0215@gmail.com>
2024-05-09 23:44:58 +0900

    feat: 카테고리 페이지 헤더 추가

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,10 @@
+import CategoryHeader from '@/components/category/CategoryHeader';
 import BottomNavigation from '@/components/common/bottomNavigation/BottomNavigation';
 
-export default function MenuPage() {
+export default function MenuListPage() {
   return (
     <div>
+      <CategoryHeader />
       <BottomNavigation />
     </div>
   );
