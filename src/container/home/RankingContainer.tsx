d815d1fb65b6099c44f57a94323a110285ed808c
sohyun <kshyun0215@gmail.com>
2024-05-10 20:26:38 +0900

    feat: dayjs locale 설정, 랭킹 타이틀 수정

--- src/container/home/RankingContainer.tsx
+++ src/container/home/RankingContainer.tsx
@@ -11,7 +11,7 @@ const RankingContainer = ({ brandList }: { brandList: CafeBrand[] }) => {
 
   return (
     <div className="flex flex-col bg-primaryIvory">
-      <div className="mt-8 pl-5 font-semibold text-gray10">오늘의 카페인 인기 랭킹</div>
+      <div className="mt-8 pl-5 font-semibold text-gray10">라떼 핏 인기 랭킹</div>
       <BrandList brandList={brandList} selectedBrand={selectedBrand} setSelectedBrand={setSelectedBrand} />
       <RankingList selectedBrand={selectedBrand} />
     </div>
