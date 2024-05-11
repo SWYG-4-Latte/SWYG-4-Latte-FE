9ad2dec11d06665651fa445346376b75dec85096
sohyun <kshyun0215@gmail.com>
2024-05-11 11:26:55 +0900

    fix: useSearchParams 훅 사용하는 NavItem을 Suspense로 감싸기

--- src/components/common/bottomNavigation/BottomNavigation.tsx
+++ src/components/common/bottomNavigation/BottomNavigation.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import NavItem from './NavItem';
 
 const BottomNavigation = () => {
@@ -5,10 +7,12 @@ const BottomNavigation = () => {
     <nav className="fixed bottom-0 left-0 right-0 z-10 mx-auto h-20 max-w-[500px] bg-primaryIvory shadow-nav">
       <div className="absolute left-0 top-0 h-px w-full bg-gray04" />
       <div className="flex justify-between px-5 pb-[30px] pt-2">
-        <NavItem path="/home" name="홈" />
-        <NavItem path="/menu" name="카테고리" />
-        <NavItem path="/calendar" name="카페인 달력" />
-        <NavItem path="/article" name="아티클" />
+        <Suspense>
+          <NavItem path="/home" name="홈" />
+          <NavItem path="/menu" name="카테고리" />
+          <NavItem path="/calendar" name="카페인 달력" />
+          <NavItem path="/article" name="아티클" />
+        </Suspense>
       </div>
     </nav>
   );
