38951c216e2ecee96db776665a1f5fbf3e40de1f
sohyun <kshyun0215@gmail.com>
2024-05-13 15:50:46 +0900

    feat: 검색 인풋에 포커스할 때 화면 확대 안되도록 수정

--- src/app/menu/search/page.tsx
+++ src/app/menu/search/page.tsx
@@ -1,6 +1,12 @@
+import type { Viewport } from 'next';
+
 import SearchMainContainer from '@/container/search/SearchMainContainer';
 
 export const dynamic = 'force-dynamic';
+
+export const viewport: Viewport = {
+  userScalable: false,
+};
 
 export default function SearchPage() {
   return (
