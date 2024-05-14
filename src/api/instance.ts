07a4fe54439482fc24132262f914e263962e9ff6
sohyun <kshyun0215@gmail.com>
2024-05-15 00:39:01 +0900

    etc: api 에러 발생 시 토큰 삭제하는 코드 주석 처리

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -25,14 +25,14 @@ apiInstance.interceptors.response.use(
     return response.data;
   },
   (error) => {
-    // 토큰이 유효하지 않은 경우 현재 500에러, 서버 측 작업 완료 후 수정 예정
-    if (error.response.status === 500) {
-      if (typeof window !== 'undefined') {
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-        window.location.reload();
-      }
-    }
+    // TODO: 토큰이 유효하지 않은 경우 저장된 토큰 삭제, 현재 500에러로 넘어와서 서버 측 작업 완료 후 수정 필요
+    // if (error.response.status === 401) {
+    //   if (typeof window !== 'undefined') {
+    //     localStorage.removeItem('accessToken');
+    //     localStorage.removeItem('refreshToken');
+    //     window.location.reload();
+    //   }
+    // }
 
     return Promise.reject(error.response);
   },
