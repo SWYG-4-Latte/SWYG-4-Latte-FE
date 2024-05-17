ecc72e735bdfac4682c2e44a91aca4da42d60ea3
sohyun <kshyun0215@gmail.com>
2024-05-17 12:17:14 +0900

    feat: 검색 - 아티클 상세페이지로 이동하는 경로 수정

--- src/components/article/ArticleHeader.tsx
+++ src/components/article/ArticleHeader.tsx
@@ -1,21 +1,14 @@
 //NEXT,React
-import Link from "next/link"
-import Image from "next/image"
-
-
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function ArticleHeader() {
   return (
-    <header className="px-5 py-4 flex items-center justify-between min-h-[56px] max-w-[500px]">
-        <h2 className="text-gray10 text-[18px] font-bold">아티클</h2>
-        <Image
-          src="/svgs/search.svg"
-          alt="search"
-          width={24}
-          height={24}
-          priority
-          unoptimized
-          />
+    <header className="flex min-h-[56px] max-w-[500px] items-center justify-between bg-primaryIvory px-5 py-4">
+      <h2 className="text-[18px] font-semibold text-gray10">아티클</h2>
+      <Link href="/article/search">
+        <Image src="/svgs/search.svg" alt="search" width={24} height={24} priority unoptimized />
+      </Link>
     </header>
-  )
+  );
 }
