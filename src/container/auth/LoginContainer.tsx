6ee25a22ade6914f8ea59b35701bb13de09789f1
sohyun <kshyun0215@gmail.com>
2024-05-17 17:13:23 +0900

    fix: 로그인 화면 로고, 오타 수정

--- src/container/auth/LoginContainer.tsx
+++ src/container/auth/LoginContainer.tsx
@@ -1,31 +1,40 @@
-'use client'
+'use client';
 // NEXT
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image';
+import Link from 'next/link';
 //Libary
-import React, { } from "react"
-import { useRouter } from "next/navigation"
-import axios from "axios"
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 // Zustand
-import useLoginStore from "@/store/loginStore"
+import useLoginStore from '@/store/loginStore';
 // Hook
-import { login } from "@/utils/auth-signup/isLogin"
-
+import { login } from '@/utils/auth-signup/isLogin';
 
 export default function LoginContainer() {
   const router = useRouter();
 
   const {
-    username, password, setUsername, setPassword,
-    usernameError, passwordError, usernameFocused, passwordFocused,
-    setUsernameFocused, setPasswordFocused, validateUsername, validatePassword,
-    setLogin, isLoggedIn
+    username,
+    password,
+    setUsername,
+    setPassword,
+    usernameError,
+    passwordError,
+    usernameFocused,
+    passwordFocused,
+    setUsernameFocused,
+    setPasswordFocused,
+    validateUsername,
+    validatePassword,
+    setLogin,
+    isLoggedIn,
   } = useLoginStore();
 
   const handleBackMove = () => {
-    router.back()
-  }
-  
+    router.back();
+  };
+
   const isInputValid = username.trim() !== '' && password.trim() !== '';
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, type: string) => {
@@ -47,39 +56,37 @@ export default function LoginContainer() {
     }
   };
 
-
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!username.trim() || !password.trim()) {
       alert('아이디와 비밀번호를 모두 입력해주세요.');
       return;
     }
-  
+
     try {
       const response = await login(username, password);
-  
+
       if (response.jwtToken && response.jwtToken.accessToken) {
         // JWT 토큰을 로컬 스토리지에 저장
         // localStorage.setItem('accessToken', response.jwtToken.accessToken);
         // localStorage.setItem('refreshToken', response.jwtToken.refreshToken);
-        setLogin(response.jwtToken.accessToken, response.jwtToken.refreshToken)
-        console.log("Login Success");
-        alert("로그인에 성공하였습니다.");
+        setLogin(response.jwtToken.accessToken, response.jwtToken.refreshToken);
+        console.log('Login Success');
+        alert('로그인에 성공하였습니다.');
         router.push('/home');
-      }
-      else {
-        console.error("Login Failed: ", response.message);
+      } else {
+        console.error('Login Failed: ', response.message);
         alert(`로그인 실패: ${response.message}`);
       }
     } catch (error) {
-      console.error("Login Error", error);
+      console.error('Login Error', error);
       alert(`로그인 오류: ${error instanceof Error ? error.message : '알 수 없는 에러가 발생했습니다.'}`);
     }
   };
 
   return (
-    <div className="flex flex-col items-center w-full h-screen text-gray10 px-5">
-      <section className="max-w-[360px] flex-i-center w-full h-[54px]">
+    <div className="flex h-screen w-full flex-col items-center px-5 text-gray10">
+      <section className="flex-i-center h-[54px] w-full max-w-[360px]">
         <Image
           onClick={handleBackMove}
           src="/svgs/svg_leftArrow.svg"
@@ -90,67 +97,63 @@ export default function LoginContainer() {
           unoptimized
         />
       </section>
-      <section className="flex-all-center mt-8 mb-4">
-        <h1 className="font-medium text-center leading-6">나에게 핏한 카페인 관리 <br />라떼핏의 조건 한 잔으로 시작해요</h1>
+      <section className="flex-all-center mb-4 mt-8">
+        <h1 className="text-center font-medium leading-snug">
+          나에게 핏한 카페인 관리 <br />
+          라떼핏의 조언 한 잔으로 시작해요
+        </h1>
       </section>
       <section className="flex-all-center mb-10">
-        <Image 
-          src="/svgs/svg_logo.svg"
-          alt="logo"
-          width={360}
-          height={80}
-          priority
-          unoptimized
-        />
+        <Image src="/svgs/svg_logo.svg" alt="logo" width={360} height={80} priority unoptimized />
       </section>
       <section className="mb-6">
-        <form
-          onSubmit={handleLogin} 
-          className="space-y-4">
+        <form onSubmit={handleLogin} className="space-y-4">
           <div className="flex flex-col justify-center">
-            <input 
+            <input
               type="text"
               value={username}
               onChange={(e) => handleInputChange(e, 'username')}
               onFocus={() => handleFocusChange('username', true)}
               onBlur={() => handleFocusChange('username', false)}
               placeholder="아이디"
-              className={`px-5 py-4 w-[320px] h-[50px] rounded-md text-[14px] text-gray10 bg-gray01 placeholder:text-gray05 outline-none
-                          border ${usernameError ? 'border-primaryRed' : (usernameFocused ? 'border-primaryOrange' : 'border-gray05')}
+              className={`h-[50px] w-[320px] rounded-md border bg-gray01 px-5 py-4 text-[14px] text-gray10 outline-none
+                          placeholder:text-gray05 ${usernameError ? 'border-primaryRed' : usernameFocused ? 'border-primaryOrange' : 'border-gray05'}
                         `}
             />
-            { usernameError &&  <p className="mt-2 text-xs text-primaryRed">{usernameError}</p> }
+            {usernameError && <p className="mt-2 text-xs text-primaryRed">{usernameError}</p>}
           </div>
           <div className="flex flex-col justify-center">
-            <input 
+            <input
               type="password"
               value={password}
               onChange={(e) => handleInputChange(e, 'password')}
               onFocus={() => handleFocusChange('password', true)}
               onBlur={() => handleFocusChange('password', false)}
               placeholder="비밀번호"
-              className={`px-5 py-4 w-[320px] h-[50px] rounded-md text-[14px] text-gray10 bg-gray01 placeholder:text-gray05 outline-none
-                          border ${passwordError ? 'border-primaryRed' : (passwordFocused ? 'border-primaryOrange' : 'border-gray05')}
+              className={`h-[50px] w-[320px] rounded-md border bg-gray01 px-5 py-4 text-[14px] text-gray10 outline-none
+                          placeholder:text-gray05 ${passwordError ? 'border-primaryRed' : passwordFocused ? 'border-primaryOrange' : 'border-gray05'}
             `}
             />
-            { passwordError && <span className="mt-2 text-xs text-primaryRed">{passwordError}</span>}
+            {passwordError && <span className="mt-2 text-xs text-primaryRed">{passwordError}</span>}
           </div>
-          <button className={`w-[320px] h-[50px] rounded-md text-gray06 ${isInputValid ? 'bg-primaryOrange text-gray10 ': 'bg-orange02 text-gray06'}`}>로그인</button>
+          <button
+            className={`h-[50px] w-[320px] rounded-md font-semibold ${isInputValid ? 'bg-primaryOrange text-gray00 ' : 'bg-orange02 text-gray06'}`}
+          >
+            로그인
+          </button>
         </form>
       </section>
       <section>
-        <div className="flex-all-center text-xs px-2">
-          <div >아이디찾기</div>
-          <div className="mx-4 w-[1px] h-[12px] border-l border-gray06"/>
-          <div >비밀번호찾기</div>
-          <div className="mx-4 w-[1px] h-[12px] border-l border-gray06"/>
-          <div >
-            <Link href="/auth/signup">
-              회원가입
-            </Link>
+        <div className="flex-all-center px-2 text-xs">
+          <div>아이디찾기</div>
+          <div className="mx-4 h-[12px] w-[1px] border-l border-gray06" />
+          <div>비밀번호찾기</div>
+          <div className="mx-4 h-[12px] w-[1px] border-l border-gray06" />
+          <div>
+            <Link href="/auth/signup">회원가입</Link>
           </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
