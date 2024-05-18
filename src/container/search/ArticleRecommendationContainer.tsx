cc6b7aab449728a42abd5a9532daf0833310a2f8
sohyun <kshyun0215@gmail.com>
2024-05-18 15:19:30 +0900

    fix: 아티클 검색 - 추천 아티클 하나만 랜덤으로 출력

--- src/container/search/ArticleRecommendationContainer.tsx
+++ src/container/search/ArticleRecommendationContainer.tsx
@@ -3,20 +3,21 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import { getArticleSearchResult } from '@/api/search';
-import { ArticleListSkeleton } from '@/components/common/skeleton/ArticleListSkeleton';
+import { RecommendedArticleItemSkeleton } from '@/components/common/skeleton/ArticleSkeleton';
 import { Article } from '@/types/article/article';
 
 const ArticleRecommendationContainer = () => {
   const router = useRouter();
 
-  const [articleList, setArticleList] = useState<Article[]>([]);
+  const [recommendedArticle, setRecommendedArticle] = useState<Article | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   const getArticleList = async () => {
     setIsLoading(true);
-    const data = await getArticleSearchResult(null, 0, 8);
+    const { content } = await getArticleSearchResult(null, 0, 8);
 
-    setArticleList(data.content);
+    const randomNum = Math.floor(Math.random() * content.length);
+    setRecommendedArticle(content[randomNum]);
     setIsLoading(false);
   };
 
@@ -27,35 +28,35 @@ const ArticleRecommendationContainer = () => {
   return (
     <div className="px-5 py-4">
       <h1 className="pt-4 font-semibold text-gray10">이런 아티클은 어때요?</h1>
-      {isLoading && <ArticleListSkeleton />}
-      {articleList.map((article) => (
+      {isLoading && <RecommendedArticleItemSkeleton />}
+      {recommendedArticle && (
         <div
-          key={article.articleNo}
+          key={recommendedArticle.articleNo}
           className="my-4 flex cursor-pointer flex-col gap-3"
-          onClick={() => router.push(`/article/detail/${article.articleNo}`)}
+          onClick={() => router.push(`/article/detail/${recommendedArticle.articleNo}`)}
         >
           <Image
             priority
             quality={100}
-            src={article.images.imgUrl2}
-            alt={article.title}
+            src={recommendedArticle.images.imgUrl2}
+            alt={recommendedArticle.title}
             width={0}
             height={0}
             sizes="100vw"
             className="h-40 w-full rounded-lg object-cover"
           />
           <div className="flex flex-col gap-2">
-            <span className="line-clamp-1 font-medium text-gray10">{article.title}</span>
+            <span className="line-clamp-1 font-medium text-gray10">{recommendedArticle.title}</span>
             <div className="flex items-center text-sm text-gray08">
               <span>조회수</span>
-              <span className="ml-[10px]">{article.viewCnt}</span>
+              <span className="ml-[10px]">{recommendedArticle.viewCnt}</span>
               <div className="mx-[10px] h-3 w-px bg-gray06" />
               <span>추천해요</span>
-              <span className="ml-[10px]">{article.likeCnt}</span>
+              <span className="ml-[10px]">{recommendedArticle.likeCnt}</span>
             </div>
           </div>
         </div>
-      ))}
+      )}
     </div>
   );
 };
