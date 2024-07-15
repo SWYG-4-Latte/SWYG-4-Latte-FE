8a7ce245bfd7659b4e8fe4b4e8bd25e75dcdd6a1
sohyun <kshyun0215@gmail.com>
2024-07-15 20:38:34 +0900

    fix: 아티클 추천 취소 기능 원래대로 수정

--- src/components/article/ArticleDetail.tsx
+++ src/components/article/ArticleDetail.tsx
@@ -93,35 +93,29 @@ const ArticleDetail = ({ article }: ArticleDetailProps) => {
           <div className="flex flex-col items-center justify-center gap-4">
             <Image src="/svgs/svg_article-smile.svg" alt="article-smile" width={40} height={40} priority className="" />
             <div>
-              <p className={cn('mb-1 text-center text-[18px] font-bold leading-[27px]', liked && 'mb-4')}>
-                {liked ? (
-                  <span className="text-center ">
-                    더 유익한 정보를 <br />
-                    드릴 수 있게 노력할게요.
-                  </span>
-                ) : (
-                  '이 아티클이 도움이 되었나요?'
-                )}
+              <p className="mb-1 text-center text-[18px] font-bold leading-[27px]">
+                {liked ? '더 유익한 정보를 위해 노력할게요.' : '이 아티클이 도움이 되었나요?'}
               </p>
               <p className="text-center text-gray08">{likeCount}명이 도움을 받았어요.</p>
             </div>
 
-            {!liked && (
-              <button
-                onClick={handleLikeClick}
-                className="flex items-center rounded-lg bg-primaryOrange px-4 py-2 text-[14px] font-medium text-gray00"
-              >
-                <Image
-                  src="/svgs/svg_article-thumb01.svg"
-                  alt="article-smile"
-                  width={16}
-                  height={16}
-                  priority
-                  className="mr-1"
-                />
-                이 아티클을 추천해요
-              </button>
-            )}
+            <button
+              onClick={handleLikeClick}
+              className={cn(
+                'flex items-center rounded-lg bg-primaryOrange px-4 py-2 text-[14px] font-medium text-gray00',
+                liked && 'bg-orange03',
+              )}
+            >
+              <Image
+                src="/svgs/svg_article-thumb01.svg"
+                alt="article-smile"
+                width={16}
+                height={16}
+                priority
+                className="mr-1"
+              />
+              {liked ? '이 아티클을 추천했어요' : '이 아티클을 추천해요'}
+            </button>
           </div>
         </aside>
       </main>
